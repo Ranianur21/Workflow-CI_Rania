d6083a5524b5ac26c49a5f1c770d4ef16539ab6a
Rania Nurbaity <ranianur21@gmail.com>
2025-12-23 20:45:20 +0700

    Add CI workflow

---
+++ modelling.py
@@ -0,0 +1,22 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+X, y = load_breast_cancer(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
