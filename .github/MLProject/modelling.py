c9c422c4259e1be1e587be875ea63dcb281b1653
Rania Nurbaity <ranianur21@gmail.com>
2025-12-24 14:36:42 +0700

    rev 1

--- .github/MLProject/modelling.py
+++ .github/MLProject/modelling.py
@@ -1,24 +1,25 @@
-import mlflow
-import mlflow.sklearn
-from sklearn.datasets import load_breast_cancer
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import mlflow
 
-X, y = load_breast_cancer(return_X_y=True)
+mlflow.autolog()
+
+df = pd.read_csv("breast_cancer_preprocessing.csv")
+
+X = df.drop(columns=["diagnosis"])
+y = df["diagnosis"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
+with mlflow.start_run(run_name="CI_LogReg"):
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
-
-    print(f"Accuracy: {acc}")  
+    print("Accuracy:", acc)
